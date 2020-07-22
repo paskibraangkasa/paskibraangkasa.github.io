d3701cd0bb1227ff28cf11fbabfc6e0dc230061d
paskibraangkasa <angkasapaskib@gmail.com>
2020-07-22 11:17:11 +0800

    initial commit

---
+++ admin/db.php
@@ -0,0 +1,4 @@
+<?php
+$con = mysqli_connect("localhost","root","","paskib") or die(mysql_error());
+
+?>
---
+++ admin/reservation.php
@@ -0,0 +1,206 @@
+<?php
+include('db.php')
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PENDAFTARAN ANGGOTA PASKIBRA ANGKASA</title>
+	<!-- Bootstrap Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- Custom Styles-->
+    <link href="assets/css/custom-styles.css" rel="stylesheet" />
+     <!-- Google Fonts-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a  href="../index.php"><i class="fa fa-home"></i> Homepage</a>
+                    </li>
+                    
+					</ul>
+
+            </div>
+
+        </nav>
+       
+        <div id="page-wrapper" >
+            <div id="page-inner">
+			 <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                            PENDAFTARAN<small></small>
+                        </h1>
+                    </div>
+                </div> 
+                 
+                                 
+            <div class="row">
+                
+                <div class="col-md-5 col-sm-5">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            DATA PRIBADI
+                      
+                              </div>
+							  <div class="form-group">
+                                            <label>Nama Depan</label>
+                                            <input name="fname" class="form-control" required>
+                                            
+                               </div>
+							   <div class="form-group">
+                                            <label>Nama Belakang</label>
+                                            <input name="lname" class="form-control" required>
+                                            
+                               </div>
+							   <div class="form-group">
+                                            <label>Email</label>
+                                            <input name="email" type="email" class="form-control" required>
+                                            
+                               </div>
+							   <div class="form-group">
+                                            <label>Jenis Kelamin*</label>
+                                            <label class="radio-inline">
+                                                <input type="radio" name="nation"  value="Pria" checked="">Pria
+                                            </label>
+                                            <label class="radio-inline">
+                                                <input type="radio" name="nation"  value="Wanita">Wanita 
+                                            </label>
+                         
+                                  </div>
+                        <div class="panel-body">
+                        <form name="form" method="post">
+                            <div class="form-group">
+                                            <label>Agama*</label>
+                                            <select name="title" class="form-control" required >
+                                                <option value selected ></option>
+                                                <option value="Islam">Islam</option>
+                                                <option value="KristenK">Kristen Katolik.</option>
+                                                <option value="KristenP">Kristen Protestan</option>
+                                                <option value="Budha">Budha</option>
+                                                <option value="Hindu">Hindu</option>
+                                            </select>
+                                        </div>
+								<?php
+
+								$kelas = array("X Mia 1", "X Mia 2", "X Mia 3", "X Mia 4", "X Mia 5", "X Iis 1", "X Iis 2",
+                                "XI Mia 1", "XI Mia 2", "XI Mia 3", "XI Mia 4", "XI Mia 5", "XI Iis 1", "XI Iis 2",
+                                "XII Mia 1", "XII Mia 2", "XII Mia 3", "XII Mia 4", "XII Mia 5", "XII Iis 1", "XII Iis 2", );
+
+								?>
+								<div class="form-group">
+                                            <label>Kelas*</label>
+                                            <select name="country" class="form-control" required>
+												<option value selected ></option>
+                                                <?php
+												foreach($kelas as $key => $value):
+												echo '<option value="'.$value.'">'.$value.'</option>'; //close your tags!!
+												endforeach;
+												?>
+                                            </select>
+								</div>
+								<div class="form-group">
+                                            <label>No. HP</label>
+                                            <input name="phone" type ="text" class="form-control" required>
+                                            
+                               </div>
+							   
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                  
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <div class="panel panel-primary">
+                       
+				
+				
+                <div class="col-md-12 col-sm-12">
+                    <div class="well">
+                        <h4>HUMAN VERIFICATION</h4>
+                        <p>Type Below this code <?php $Random_code=rand(); echo$Random_code; ?> </p><br />
+						<p>Enter the random code<br /></p>
+							<input  type="text" name="code1" title="random code" />
+							<input type="hidden" name="code" value="<?php echo $Random_code; ?>" />
+						<input type="submit" name="submit" class="btn btn-primary">
+						<?php
+							if(isset($_POST['submit']))
+							{
+							$code1=$_POST['code1'];
+							$code=$_POST['code']; 
+							if($code1!="$code")
+							{
+							$msg="Invalide code"; 
+							}
+							else
+							{
+							
+									$con=mysqli_connect("localhost","root","","paskib");
+									$check="SELECT * FROM roombook WHERE email = '$_POST[email]'";
+									$rs = mysqli_query($con,$check);
+									$data = mysqli_fetch_array($rs, MYSQLI_NUM);
+									if($data[0] > 1) {
+										echo "<script type='text/javascript'> alert('User Already in Exists')</script>";
+										
+									}
+
+									else
+									{
+										$new ="Not Conform";
+										$newUser="INSERT INTO `roombook`(`FName`, `LName`, `Email`, `National`, `Country`, `Phone`) VALUES ('$_POST[title]','$_POST[fname]','$_POST[lname]','$_POST[email]','$_POST[nation]','$_POST[country]','$_POST[phone]'))";
+										if (mysqli_query($con,$newUser))
+										{
+											echo "<script type='text/javascript'> alert('Your Booking application has been sent')</script>";
+											
+										}
+										else
+										{
+											echo "<script type='text/javascript'> alert('Error adding user in database')</script>";
+											
+										}
+									}
+
+							$msg="Your code is correct";
+							}
+							}
+							?>
+						</form>
+							
+                    </div>
+                </div>
+            </div>
+           
+                
+                </div>
+                    
+            
+				
+					</div>
+			 <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+      <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script>
+    
+   
+</body>
+</html>
